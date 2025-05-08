2e0687595b452fb39eb9df101c5520502ee37426
kiranLoka <kirankumarloka@gmail.com>
2025-05-08 13:02:33 +0530

    sample navbar

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,3 +1,29 @@
+import {
+  Navbar,
+  NavBody,
+  NavbarButton,
+  NavbarLogo,
+  NavItems,
+} from "@/components/ui/resizable-navbar";
+
 export default function Home() {
-  return <div className="text-white text-2xl">hello this is kiran </div>;
+  const navItems = [
+    { name: "Home", link: "/" },
+    { name: "Features", link: "/features" },
+    { name: "Pricing", link: "/pricing" },
+    { name: "Contact", link: "/contact" },
+  ];
+
+  return (
+    <div>
+      <Navbar className="top-0">
+        <NavBody>
+          <NavbarLogo />
+          <NavItems items={navItems} />
+          <NavbarButton href="/login">Login</NavbarButton>
+        </NavBody>
+      </Navbar>
+      <div className="text-white text-2xl">hello this is kiran </div>
+    </div>
+  );
 }
